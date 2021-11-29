59bcaa06221cd3b0e1d087d7bbc37960f4074fef
Daniel Mocko <daniel.mocko@panonit.com>
2021-11-29 20:19:26 +0100

    first commit

---
+++ src/main/java/com/hamwebtest/HamwebtestApplication.java
@@ -0,0 +1,13 @@
+package com.hamwebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamwebtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamwebtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamwebtest/HamwebtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamwebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamwebtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
