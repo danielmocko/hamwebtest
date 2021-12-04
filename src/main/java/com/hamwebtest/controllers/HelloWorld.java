c345ba9e1361abb7e73aeba6378ada96bb5cc2fa
Daniel Mocko <daniel.mocko@panonit.com>
2021-12-04 21:09:30 +0100

    git add .!

--- src/main/java/com/hamwebtest/controllers/HelloWorld.java
+++ src/main/java/com/hamwebtest/controllers/HelloWorld.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloWorld {
 
     @GetMapping("/hello")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World!!!") String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
     }
